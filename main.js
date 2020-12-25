ce5770c9e1e3ddb3e08787dca93656a26e112676
grenudi <idunerg@gmail.com>
2020-12-25 03:04:57 +0300

    init

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const pipe = (...args)=>args.reduce((a,x)=>x(a));
-const wrap = (f)=>(...args)=>f(...args);
-const chain = (args,...f)=>f.map(x=>wrap(x)(...args))
-const split = ()=>"nothinghere yet" // 
 
 (function(){
     "use strict";
+    const pipe = (...args)=>args.reduce((a,x)=>x(a));
+    const wrap = (f)=>(...args)=>f(...args);
+    const chain = (args,...f)=>f.map(x=>wrap(x)(...args))
+    const split = ()=>"nothinghere yet" // 
  
     this.Calc = function () {
         return Object.freeze({
