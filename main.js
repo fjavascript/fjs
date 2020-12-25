e7c2f5710139097e3aa44e210d544104219ebcab
grenudi <idunerg@gmail.com>
2020-12-25 10:48:21 +0300

    init

--- main.js
+++ main.js
@@ -4,13 +4,20 @@
     const wrap = (f)=>(...args)=>f(...args);
     const chain = (args,...f)=>f.map(x=>wrap(x)(...args))
     const split = ()=>"nothinghere yet" // 
+    const changePx = (fP,fn)=> x=> fP(x) ? fn(x) : x;
+    const andPx = fP=> (...args)=> cb=> x=>{
+      const res = args.map(y=>y(x)).filter(y=>fP(x, y));
+      return res.length === args.length ? cb(res) : cb(false, x);
+    }
  
     this.Calc = function () {
         return Object.freeze({
           pipe,
           wrap,
           chain,
-          split
+          split,
+          changePx,
+          andPx
         });
         
     }();
